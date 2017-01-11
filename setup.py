5fccd9b77b2ab6993e3cfd96f88f428de2c8da81
Alejandro Caceres <acaceres@hyperiongray.com>
2017-01-10 23:56:22 -0500

    release .0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dropchat',
-    version = '0.1.0',
+    version = '0.2.1',
     description = 'Drop Chat',
     long_description = 'Drop Chat - Secure Disposable Chat',
     url = '',
